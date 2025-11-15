884eac22afe063c935179501a45f44604b63c0ec
ronaldoucl <ronaldocl9lda@gmail.com>
2025-11-15 13:52:59 -0600

    save changes

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -103,15 +103,14 @@ async function seedRevenue() {
 
 export async function GET() {
   try {
-    const result = await sql.begin((sql) => [
-      seedUsers(),
-      seedCustomers(),
-      seedInvoices(),
-      seedRevenue(),
-    ]);
+    await seedUsers();
+    await seedCustomers();
+    await seedInvoices();
+    await seedRevenue();
 
     return Response.json({ message: 'Database seeded successfully' });
   } catch (error) {
+    console.error(error);
     return Response.json({ error }, { status: 500 });
   }
 }
